2dcbbaa7feba53214cad00ddb6d77567d76c4bcf
Jiří Sejkora <sejsel@sejsel.cz>
2024-02-17 21:35:14 +0100

    Improve the CLI usage
    Now used as `./ksplang-cli program.ksplang` with input on stdin.

--- ksplang-cli/src/main.rs
+++ ksplang-cli/src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use ksplang::ops::Op;
 use ksplang::parser;
 use ksplang::vm::VMOptions;
@@ -11,35 +12,60 @@ fn get_pi_digits() -> Vec<i8> {
         .collect()
 }
 
-const STACK_SEPARATOR: &str = "!";
-const MAX_OPS: u64 = 1_000_000_000;
-const MAX_STACK_SIZE: usize = 2_097_152;
+/// Run a ksplang program.
+#[derive(Parser, Debug)]
+#[command()]
+struct Args {
+    /// File containing a ksplang program.
+    #[arg()]
+    file: String,
+    /// Maximum stack size.
+    #[arg(long, short = 's', default_value_t = 2097152)]
+    max_stack_size: usize,
+    /// A limit for the number of executed operations.
+    /// If the limit is reached, the program will be stopped with an error.
+    #[arg(long, short = 'l')]
+    op_limit: Option<u64>,
+}
 
-fn main() -> anyhow::Result<()> {
-    let pi_digits = get_pi_digits();
+fn read_program_from_file(file: &str) -> Result<Vec<Op>, anyhow::Error> {
+    let file = std::fs::File::open(file)?;
+    let reader = std::io::BufReader::new(file);
     let mut ops: Vec<Op> = Vec::new();
-    let mut in_stack = false;
-    let mut stack: Vec<i64> = Vec::new();
-    for line in std::io::stdin().lock().lines() {
-        let line = line.expect("Failed to read a line from stdin.");
+    for line in reader.lines() {
+        let line = line?;
         for word in line.split_whitespace() {
-            if !in_stack {
-                if word == STACK_SEPARATOR {
-                    in_stack = true;
-                } else {
-                    ops.push(parser::parse_word(&word)?);
-                }
-            } else {
-                if word == STACK_SEPARATOR {
-                    return Err(anyhow::anyhow!("Found two stack separators."));
-                } else {
-                    stack.push(word.parse()?);
-                }
-            }
+            ops.push(parser::parse_word(&word)?);
         }
     }
+    Ok(ops)
+}
 
-    let options = VMOptions::new(&stack, MAX_STACK_SIZE, &pi_digits, MAX_OPS, u64::MAX);
+fn read_stack_from_stdin() -> Result<Vec<i64>, anyhow::Error> {
+    let mut stack: Vec<i64> = Vec::new();
+    for line in std::io::stdin().lock().lines() {
+        let line = line?;
+        for word in line.split_whitespace() {
+            stack.push(word.parse()?);
+        }
+    }
+    Ok(stack)
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let ops: Vec<Op> = read_program_from_file(&args.file)?;
+    let stack: Vec<i64> = read_stack_from_stdin()?;
+
+    let pi_digits = get_pi_digits();
+    let options = VMOptions::new(
+        &stack,
+        args.max_stack_size,
+        &pi_digits,
+        args.op_limit.unwrap_or(u64::MAX),
+        u64::MAX,
+    );
     let result = ksplang::vm::run(&ops, options)?;
     for &value in result.stack.iter() {
         println!("{}", value);
