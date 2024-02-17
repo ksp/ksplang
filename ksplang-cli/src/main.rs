9c4a1e3c5af8df0b0aa3e645012a1ead8eb4951b
Jiří Sejkora <sejsel@sejsel.cz>
2024-02-17 21:55:46 +0100

    CLI: Add stats

--- ksplang-cli/src/main.rs
+++ ksplang-cli/src/main.rs
@@ -3,6 +3,7 @@ use ksplang::ops::Op;
 use ksplang::parser;
 use ksplang::vm::VMOptions;
 use std::io::BufRead;
+use std::time::Duration;
 
 fn get_pi_digits() -> Vec<i8> {
     include_str!("../../pi-10million.txt")
@@ -20,12 +21,15 @@ struct Args {
     #[arg()]
     file: String,
     /// Maximum stack size.
-    #[arg(long, short = 's', default_value_t = 2097152)]
+    #[arg(long, short = 'm', default_value_t = 2097152)]
     max_stack_size: usize,
     /// A limit for the number of executed operations.
     /// If the limit is reached, the program will be stopped with an error.
     #[arg(long, short = 'l')]
     op_limit: Option<u64>,
+    /// Print statistics after running the program.
+    #[arg(long, short = 's')]
+    stats: bool,
 }
 
 fn read_program_from_file(file: &str) -> Result<Vec<Op>, anyhow::Error> {
@@ -66,10 +70,32 @@ fn main() -> anyhow::Result<()> {
         args.op_limit.unwrap_or(u64::MAX),
         u64::MAX,
     );
+
+    let start_time = std::time::Instant::now();
     let result = ksplang::vm::run(&ops, options)?;
+    let elapsed = start_time.elapsed();
+
+    if args.stats {
+        print_stats(result.instruction_counter, elapsed);
+    }
+
     for &value in result.stack.iter() {
         println!("{}", value);
     }
 
     Ok(())
+}
+
+fn print_stats(instruction_counter: u64, elapsed: Duration) {
+    let instructions_per_second = instruction_counter as f64 / elapsed.as_secs_f64();
+    eprintln!("Execution time: {:?}", elapsed);
+    eprintln!(
+        "Instructions executed: {} ({}/s)",
+        instruction_counter,
+        match instructions_per_second {
+            n if n >= 1_000_000.0 => format!("{:.1}M", n / 1_000_000.0),
+            n if n >= 1_000.0 => format!("{:.1}k", n / 1_000.0),
+            n => format!("{:.1}", n),
+        }
+    );
 }
